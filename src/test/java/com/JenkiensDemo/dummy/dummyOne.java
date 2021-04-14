1d90075cf8ee28fd9269dbd6d559ad42bc61ff01
Akash Singh <74761354+akash9936@users.noreply.github.com>
2021-04-15 02:50:55 +0530

    dummytwopush

--- src/test/java/com/JenkiensDemo/dummy/dummyOne.java
+++ src/test/java/com/JenkiensDemo/dummy/dummyOne.java
@@ -1,15 +1,18 @@
-package com.dummy;
+package com.JenkiensDemo.dummy;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import junit.framework.Assert;
+
 public class dummyOne {
 
 
-public class NewTest {
+public class NewTestStartedDummy {
   @Test
   public void TestClass() {
 	  System.out.println("We are in Test");
+	  Assert.assertTrue(true);
   }
   @BeforeMethod
   public void beforeMethod() {
