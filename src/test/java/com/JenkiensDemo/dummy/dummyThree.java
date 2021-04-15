c7292d6b7ed35b0cb95d15a81810c4774cb15438
Akash Singh <74761354+akash9936@users.noreply.github.com>
2021-04-15 13:46:32 +0530

    Commited Class DummyThree

---
+++ src/test/java/com/JenkiensDemo/dummy/dummyThree.java
@@ -0,0 +1,11 @@
+package com.JenkiensDemo.dummy;
+
+import org.testng.annotations.Test;
+
+public class dummyThree {
+	@Test
+public void dummyThreeDel() {
+	System.out.println("We are in dummy three");
+	
+}
+}
---
+++ src/test/java/com/JenkiensDemo/dummy/dummyTwo.java
@@ -0,0 +1,11 @@
+package com.JenkiensDemo.dummy;
+
+import org.testng.annotations.Test;
+
+public class dummyTwo {
+@Test
+private void dummytwodel() {
+	System.out.println("Dummy Twon");
+
+}
+}
